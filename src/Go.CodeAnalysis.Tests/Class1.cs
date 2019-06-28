456c2e6ca3cc2a3254331b13c5604122d82a092e
Christian Gunderman <gundermanc@gmail.com>
2019-06-27 20:41:41 -0700

    Skeleton for full language service.

---
+++ src/Go.CodeAnalysis.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Go.CodeAnalysis.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
