5de3135aa1e9fac614e908f1e35b0a3c52835ad9
benhxy <ben.huxiaoye@gmail.com>
2019-07-13 20:16:23 -0700

    Update logic

--- src/Go.CodeAnalysis/Parser/ImportDeclarationNode.cs
+++ src/Go.CodeAnalysis/Parser/ImportDeclarationNode.cs
@@ -1,11 +1,7 @@
 ﻿namespace Go.CodeAnalysis.Parser
 {
-    using System;
     using System.Collections.Generic;
     using System.Collections.Immutable;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using Go.CodeAnalysis.Common;
     using Go.CodeAnalysis.Lex;
     using Go.CodeAnalysis.Text;
@@ -15,16 +11,23 @@
     /// </summary>
     public class ImportDeclarationNode : ParseNode
     {
-        public ImportDeclarationNode(SnapshotSegment code)
-            : base(code, new ImmutableArray<ParseNode>())
+        public ImportDeclarationNode(
+            SnapshotSegment code,
+            string packageName,
+            string alias = null)
+            : base(code, ImmutableArray<ParseNode>.Empty)
         {
         }
+
+        public string PackageName { get; private set; }
+
+        public string PackageAlias { get; private set; }
 
         public static bool TryParse(Lexer lexer, IList<Error> errors, out ImportDeclarationNode parseNode)
         {
             // to parse: alias/wildcard import
             // to parse: package name
-            parseNode = new ImportDeclarationNode(lexer.CurrentLexeme.Extent);
+            parseNode = new ImportDeclarationNode(lexer.CurrentLexeme.Extent, string.Empty, string.Empty);
             return true;
         }
     }
