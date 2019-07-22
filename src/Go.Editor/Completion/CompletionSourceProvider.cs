be7c95e59f9216219a43be1ddd36c51c1ff7bfb8
Katarzyna Bulat <t-kabul@microsoft.com>
2019-07-22 12:31:23 -0700

    sample completion copied

---
+++ src/Go.Editor/Completion/CompletionSourceProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.ComponentModel.Composition;
+
+namespace Go.Editor.Completion
+{
+    [Export(typeof(IAsyncCompletionSourceProvider))]
+    [ContentType("CSharp")]
+    [Name("Completion item source")]
+    public class CompletionSourceProvider : IAsyncCompletionSourceProvider
+    {
+        Lazy<CompletionSource> Source = new Lazy<CompletionSource>(() => new CompletionSource());
+
+        public IAsyncCompletionSource GetOrCreate(ITextView textView)
+        {
+            return Source.Value;
+        }
+    }
+}
