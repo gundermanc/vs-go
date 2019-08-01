b4522254b23a1990343d855cc04449924132e7b6
Alexander <10926386+atelich139@users.noreply.github.com>
2019-07-31 21:09:24 -0400

    Cleaned up formatting

--- src/go/bindings/bindings.go
+++ src/go/bindings/bindings.go
@@ -4,96 +4,97 @@ package main
 // #include "bindings.h"
 import "C"
 import (
-	"go/languageservice"
-	"io"
-	"unsafe"
+    "io"
+    "unsafe"
+
+    "vs-go/src/go/languageservice"
 )
 
 type snapshotReader struct {
-	Snapshot C.Snapshot
-	Offset   int
+    Snapshot C.Snapshot
+    Offset   int
 }
 
 func (snapshot C.Snapshot) newReader() *snapshotReader {
-	return &snapshotReader{snapshot, 0}
+    return &snapshotReader{snapshot, 0}
 }
 
 // Read reads bytes from a snapshotReader into a buffer.
 func (reader *snapshotReader) Read(buffer []byte) (n int, err error) {
 
-	if reader.Offset >= int(reader.Snapshot.length) {
-		return 0, io.EOF
-	}
+    if reader.Offset >= int(reader.Snapshot.length) {
+        return 0, io.EOF
+    }
 
-	read := int(C.Read(reader.Snapshot, (*C.uint8_t)(&buffer[0]), C.int(reader.Offset), C.int(len(buffer))))
-	reader.Offset += read
+    read := int(C.Read(reader.Snapshot, (*C.uint8_t)(&buffer[0]), C.int(reader.Offset), C.int(len(buffer))))
+    reader.Offset += read
 
-	return read, nil
+    return read, nil
 }
 
 //export CreateNewWorkspace
 func CreateNewWorkspace() int32 {
-	return int32(languageservice.CreateNewWorkspace())
+    return int32(languageservice.CreateNewWorkspace())
 }
 
 //export RegisterWorkspaceUpdateCallback
 func RegisterWorkspaceUpdateCallback(workspaceID int32, callback C.ProvideStringCallback) {
 
-	goCallback := func(fileName string) {
-		fileNameSlice := []byte(fileName)
-		C.InvokeStringCallback(callback, (*C.uint8_t)(&fileNameSlice[0]), C.int(len(fileNameSlice)))
-	}
+    goCallback := func(fileName string) {
+        fileNameSlice := []byte(fileName)
+        C.InvokeStringCallback(callback, (*C.uint8_t)(&fileNameSlice[0]), C.int(len(fileNameSlice)))
+    }
 
-	languageservice.WorkspaceID(workspaceID).RegisterWorkspaceUpdateCallback(goCallback)
+    languageservice.WorkspaceID(workspaceID).RegisterWorkspaceUpdateCallback(goCallback)
 }
 
 //export QueueFileParse
 func QueueFileParse(workspaceID int32, fileName *byte, count int32, snapshot C.Snapshot) {
-	reader := snapshot.newReader()
-	fileNameString := cToString(fileName, count)
-	languageservice.WorkspaceID(workspaceID).QueueFileParse(fileNameString, reader)
+    reader := snapshot.newReader()
+    fileNameString := cToString(fileName, count)
+    languageservice.WorkspaceID(workspaceID).QueueFileParse(fileNameString, reader)
 }
 
 //export GetWorkspaceCompletions
 func GetWorkspaceCompletions(workspaceID int, callback C.ProvideStringCallback, position int) {
-	completions, _ := languageservice.WorkspaceID(workspaceID).GetCompletions(position)
+    completions, _ := languageservice.WorkspaceID(workspaceID).GetCompletions(position)
 
-	for _, completion := range completions {
+    for _, completion := range completions {
 
-		completionText := []byte(completion)
+        completionText := []byte(completion)
 
-		C.InvokeStringCallback(callback, (*C.uint8_t)(&completionText[0]), C.int(len(completionText)))
-	}
+        C.InvokeStringCallback(callback, (*C.uint8_t)(&completionText[0]), C.int(len(completionText)))
+    }
 }
 
 //export GetTokens
 func GetTokens(workspaceID int32, fileName *byte, count int32, callback C.ProvideTokenCallback) {
 
-	fileNameString := cToString(fileName, count)
+    fileNameString := cToString(fileName, count)
 
-	tokens, err := languageservice.WorkspaceID(workspaceID).GetTokens(fileNameString)
-	if err == nil {
-		for _, token := range tokens {
-			C.InvokeTokenCallback(callback, C.int32_t(token.Pos), C.int32_t(token.End), C.int32_t(token.Type))
-		}
-	}
+    tokens, err := languageservice.WorkspaceID(workspaceID).GetTokens(fileNameString)
+    if err == nil {
+        for _, token := range tokens {
+            C.InvokeTokenCallback(callback, C.int32_t(token.Pos), C.int32_t(token.End), C.int32_t(token.Type))
+        }
+    }
 }
 
 //export GetWorkspaceErrors
 func GetWorkspaceErrors(workspaceID int32, callback C.ProvideStringCallback) {
-	rawErrors := languageservice.WorkspaceID(workspaceID).GetWorkspaceErrors()
+    rawErrors := languageservice.WorkspaceID(workspaceID).GetWorkspaceErrors()
 
-	for _, err := range rawErrors {
+    for _, err := range rawErrors {
 
-		errorText := []byte(err.Error())
+        errorText := []byte(err.Error())
 
-		C.InvokeStringCallback(callback, (*C.uint8_t)(&errorText[0]), C.int(len(errorText)))
-	}
+        C.InvokeStringCallback(callback, (*C.uint8_t)(&errorText[0]), C.int(len(errorText)))
+    }
 }
 
 func cToString(bytes *byte, length int32) string {
-	// TODO: there are 2 copies being made here. Eliminate one.
-	return string(C.GoBytes(unsafe.Pointer(bytes), C.int(length)))
+    // TODO: there are 2 copies being made here. Eliminate one.
+    return string(C.GoBytes(unsafe.Pointer(bytes), C.int(length)))
 }
 
 // Note: The "C" library requires there to be main
--- src/go/languageservice/workspacedocument.go
+++ src/go/languageservice/workspacedocument.go
@@ -4,98 +4,98 @@
 package languageservice
 
 import (
-	"go/ast"
-	"go/importer"
-	"go/parser"
-	"go/token"
-	"go/types"
-	"io"
-	"sync"
+    "go/ast"
+    "go/importer"
+    "go/parser"
+    "go/token"
+    "go/types"
+    "io"
+    "sync"
 )
 
 type workspaceDocument struct {
-	QueuedReparses int
-	Mutex          *sync.Mutex // TODO: consider replacing mutex with channel?
-	File           *ast.File
-	Error          []error
-    FileSet         *token.FileSet
+    QueuedReparses int
+    Mutex          *sync.Mutex // TODO: consider replacing mutex with channel?
+    File           *ast.File
+    Error          []error
+    FileSet        *token.FileSet
 }
 
 func createNewWorkspaceDocument(fileName string) *workspaceDocument {
-	return &workspaceDocument{
-		QueuedReparses: 0,
-		Mutex:          &sync.Mutex{},
-		File:           nil,
-		Error:          nil,
-	}
+    return &workspaceDocument{
+        QueuedReparses: 0,
+        Mutex:          &sync.Mutex{},
+        File:           nil,
+        Error:          nil,
+    }
 }
 
 func (document *workspaceDocument) queueReparse(fileName string, reader io.Reader, callback WorkspaceUpdateCallback) {
 
-	document.Mutex.Lock()
-	document.QueuedReparses++
-	oldQueuedReparses := document.QueuedReparses
-	document.Mutex.Unlock()
+    document.Mutex.Lock()
+    document.QueuedReparses++
+    oldQueuedReparses := document.QueuedReparses
+    document.Mutex.Unlock()
 
-	if oldQueuedReparses == 1 {
-		go document.reparseAll(fileName, reader, callback)
-	}
+    if oldQueuedReparses == 1 {
+        go document.reparseAll(fileName, reader, callback)
+    }
 }
 
 func (document *workspaceDocument) reparseAll(fileName string, reader io.Reader, callback WorkspaceUpdateCallback) {
-	for true {
+    for true {
 
-		document.Mutex.Lock()
+        document.Mutex.Lock()
 
-		oldQueuedReparses := document.QueuedReparses
-		document.QueuedReparses = 0
+        oldQueuedReparses := document.QueuedReparses
+        document.QueuedReparses = 0
 
-		document.Mutex.Unlock()
+        document.Mutex.Unlock()
 
-		// TODO: consider swapping the mutex for atomic/interlocked operations or channel?
-		if oldQueuedReparses > 0 {
-			document.reparse(fileName, reader)
-			callback(fileName)
-		} else {
-			return
-		}
-	}
+        // TODO: consider swapping the mutex for atomic/interlocked operations or channel?
+        if oldQueuedReparses > 0 {
+            document.reparse(fileName, reader)
+            callback(fileName)
+        } else {
+            return
+        }
+    }
 }
 
 func (document *workspaceDocument) reparse(fileName string, reader io.Reader) {
-	if reader == nil {
-		panic("reader cannot be nil")
-	}
+    if reader == nil {
+        panic("reader cannot be nil")
+    }
 
-	fileSet := token.NewFileSet()
+    fileSet := token.NewFileSet()
 
-	// Do parse.
-	f, err := parser.ParseFile(fileSet, "", reader, parser.ParseComments)
+    // Do parse.
+    f, err := parser.ParseFile(fileSet, "", reader, parser.ParseComments)
 
-	// Update document.
-	document.Mutex.Lock()
-	defer document.Mutex.Unlock()
-	document.Error = nil
-	if err != nil {
-		document.Error = append(document.Error, err)
-	}
+    // Update document.
+    document.Mutex.Lock()
+    defer document.Mutex.Unlock()
+    document.Error = nil
+    if err != nil {
+        document.Error = append(document.Error, err)
+    }
 
-	document.File = f
+    document.File = f
     document.FileSet = fileSet
 
-	// See https://github.com/golang/example/tree/master/gotypes for full example code.
+    // See https://github.com/golang/example/tree/master/gotypes for full example code.
 
-	// A Config controls various options of the type checker.
-	// The defaults work fine except for one setting:
-	// we must specify how to deal with imports.
-	conf := types.Config{Importer: importer.Default()}
+    // A Config controls various options of the type checker.
+    // The defaults work fine except for one setting:
+    // we must specify how to deal with imports.
+    conf := types.Config{Importer: importer.Default()}
 
-	// Type-check the package containing only file f.
-	// Check returns a *types.Package.
-	// TODO: at some point, use a snapshot of the workspace
-	// so we can resolve types across all files.
-	_, err = conf.Check("cmd/hello", fileSet, []*ast.File{f}, nil)
-	if err != nil {
-		document.Error = append(document.Error, err)
-	}
+    // Type-check the package containing only file f.
+    // Check returns a *types.Package.
+    // TODO: at some point, use a snapshot of the workspace
+    // so we can resolve types across all files.
+    _, err = conf.Check("cmd/hello", fileSet, []*ast.File{f}, nil)
+    if err != nil {
+        document.Error = append(document.Error, err)
+    }
 }
